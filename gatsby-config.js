170c8326d0a1b0ad50f6c0999a311b351abca8aa
JeremyBatch <jdbatchelor85@gmail.com>
2020-06-16 20:16:40 -0500

    📦  NEW: add gatsby-source-filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,27 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: 'gatsby-source-filesystem'
+      options: {
+        name: 'pages',
+        path: '${_dirname}/src/pages'
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem'
+      options: {
+        name: 'post',
+        path: '${_dirname}/src/post'
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem'
+      options: {
+        name: 'images',
+        path: '${_dirname}/src/images'
+      },
+    },
+  ],
 }
