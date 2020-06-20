408dd9ab919f303699046114f33ac6fb770d5722
JeremyBatch <jdbatchelor85@gmail.com>
2020-06-19 22:02:20 -0500

    📦  NEW: add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,64 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import {
+  FooterWrapper,
+  FooterSocialWrapper,
+  FooterSocialIcons,
+} from "../elements"
+
+export const Footer = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      facebook: file(relativePath: { eq: "facebook.svg" }) {
+        publicURL
+      }
+      linkedin: file(relativePath: { eq: "linkedin.svg" }) {
+        publicURL
+      }
+      instagram: file(relativePath: { eq: "instagram.svg" }) {
+        publicURL
+      }
+      twitter: file(relativePath: { eq: "twitter.svg" }) {
+        publicURL
+      }
+    }
+  `)
+
+  return (
+    <FooterWrapper>
+      <FooterSocialWrapper>
+        <FooterSocialIcons>
+          <a
+            href="https://www.facebook.com/jeremy.batchelor"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={data.facebook.publicURL} alt="Facebook logo" />
+          </a>
+          <a
+            href="https://www.instagram.com/jdbatchelor/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={data.instagram.publicURL} alt="Instagram logo" />
+          </a>
+          <a
+            href="www.linkedin.com/in/jeremy-batchelor-63a0b9186"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={data.linkedin.publicURL} alt="Linkedin logo" />
+          </a>
+          <a
+            href="https://twitter.com/jdbatchelor"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={data.twitter.publicURL} alt="Twitter logo" />
+          </a>
+        </FooterSocialIcons>
+        <p>@2020 Company. All rights reserved</p>
+      </FooterSocialWrapper>
+    </FooterWrapper>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import { Container } from "../components/Container"
 const IndexPage = () => {
   return (
     <Container>
-      <h1>Hello World!</h1>
+      <h1></h1>
     </Container>
   )
 }
